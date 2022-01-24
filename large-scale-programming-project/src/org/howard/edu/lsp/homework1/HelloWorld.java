35f55fbd5a822322274a6e50c0f2946f1f062fa5
SydnieS <86460345+SydnieSewell@users.noreply.github.com>
2022-01-24 16:24:13 -0500

    This is my first github commit.

---
+++ large-scale-programming-project/src/org/howard/edu/lsp/homework1/HelloWorld.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package org.howard.edu.lsp.homework1;
+
+/**
+ * @author Sydnie Sewell 
+ *
+ */
+public class HelloWorld {
+
+	
+	public static void main(String[] args) {
+		System.out.println("Hello LSP!!");
+
+	}
+
+}
